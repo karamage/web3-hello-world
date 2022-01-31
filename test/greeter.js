e31aaf6be94d9fe462362d0f054f926bd3b93c58
karamage <karamage.umauma@gmail.com>
2022-01-31 15:22:12 +0900

    add test.

--- test/greeter.js
+++ test/greeter.js
@@ -10,6 +10,15 @@ contract("Greeter", function (/* accounts */) {
     await Greeter.deployed();
     return assert.isTrue(true);
   });
+
+  describe("greet()", () => {
+    it("returns 'Hello, World!'", async () => {
+      const greeter = await Greeter.deployed();
+      const expected = "Hello, World!";
+      const actual = await greeter.greet();
+      assert.equal(actual, expected, "greeted with 'Hello, World!");
+    });
+  });
 });
 
 /*
