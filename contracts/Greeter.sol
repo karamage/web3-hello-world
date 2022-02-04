8d8bc606b096c9d5cc8355e7fcc1c943221fee56
karamage <karamage.umauma@gmail.com>
2022-02-04 15:07:37 +0900

    add setGreeting.

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -2,7 +2,13 @@
 pragma solidity ^0.8.11;
 
 contract Greeter {
-  function greet() external pure returns (string memory) {
-    return "Hello, World!";
+  string private _greeting = "Hello, World!";
+
+  function greet() external view returns (string memory) {
+    return _greeting;
+  }
+
+  function setGreeting(string calldata greeting) external {
+    _greeting = greeting;
   }
 }
