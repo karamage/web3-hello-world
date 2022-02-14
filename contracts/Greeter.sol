b9b996372fd7e5c4da56b545e37dc46a0b30f939
karamage <karamage.umauma@gmail.com>
2022-02-14 15:08:06 +0900

    add openzeppelin Ownable.

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,21 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
-contract Greeter {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Greeter is Ownable {
   string private _greeting = "Hello, World!";
-  address private _owner;
-
-  constructor() {
-    _owner = msg.sender;
-  }
-
-  modifier onlyOwner() {
-    require(
-      msg.sender == _owner,
-      "Ownable: caller is not the owner"
-    );
-    _;
-  }
 
   function greet() external view returns (string memory) {
     return _greeting;
@@ -23,9 +12,5 @@ contract Greeter {
 
   function setGreeting(string calldata greeting) external onlyOwner {
     _greeting = greeting;
-  }
-
-  function owner() public view returns(address) {
-    return _owner;
   }
 }
